2520ad3d73cd16fb8f0eb06533c485ab622cd8e9
Janowak536 <janowak536@gmail.com>
2021-12-21 18:52:40 +0100

    dodanie to do list oraz planu zajec

---
+++ WebAppBlazor/Models/ToDoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppBlazor
+{
+    public class ToDoItem 
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+        public string Nazwa { get; set; } = "";
+        public bool Stan { get; set; } = false;
+    }
+}
---
+++ WebAppBlazor/Models/Zajecia.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppBlazor
+{
+    public class Zajecia
+    {
+        public Guid Id { get; } = Guid.NewGuid();
+        public string Nazwa { get; set; } = "";
+        public DateTime Data { get; set; } 
+        public int Uczestnicy { get; set; } = 0;
+        public int UczestnicyMax { get; set; } = 10;
+    }
+}
